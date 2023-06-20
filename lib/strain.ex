fa50bec3061e0076745037c51bd7d8e05cafce32
Albert Gabitov <agabitov@evrone.com>
2023-06-20 13:35:36 +0300

    LinkedList

--- lib/strain.ex
+++ lib/strain.ex
@@ -6,7 +6,8 @@ defmodule Strain do
   Do not use `Enum.filter`.
   """
   @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
-  def keep(list, fun), do: for elem <- list, fun.(elem), do: elem
+  def keep(list, fun), do: for(elem <- list, fun.(elem), do: elem)
+
   @doc """
   Given a `list` of items and a function `fun`, return the list of items where
   `fun` returns false.
@@ -14,5 +15,5 @@ defmodule Strain do
   Do not use `Enum.reject`.
   """
   @spec discard(list :: list(any), fun :: (any -> boolean)) :: list(any)
-  def discard(list, fun), do: keep(list, &(not(fun.(&1))))
+  def discard(list, fun), do: keep(list, &(not fun.(&1)))
 end
