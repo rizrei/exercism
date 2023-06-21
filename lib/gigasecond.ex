b24dacdd76a8db6cd4eb13e7766eade913105e5b
Albert Gabitov <agabitov@evrone.com>
2023-06-21 21:53:16 +0300

    Gigasecond(NaiveDateTime)

---
+++ lib/gigasecond.ex
@@ -0,0 +1,14 @@
+defmodule Gigasecond do
+  @doc """
+  Calculate a date one billion seconds after an input date.
+  """
+
+  @spec from({{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}) ::
+          {{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}
+  def from(date) do
+    date
+    |> NaiveDateTime.from_erl!()
+    |> NaiveDateTime.add(10 ** 9)
+    |> NaiveDateTime.to_erl()
+  end
+end
