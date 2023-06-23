f6c7d2b8c849565c6470d9b9e430d7ed9ef528cf
Albert Gabitov <agabitov@evrone.com>
2023-06-23 20:58:17 +0300

    Acronym(pipe, regex)

--- lib/username.ex
+++ lib/username.ex
@@ -2,10 +2,10 @@ defmodule Username do
   @spec sanitize(charlist) :: charlist
   def sanitize([]), do: []
   def sanitize([head | tail]) when head in ?a..?z, do: [head | sanitize(tail)]
-  def sanitize([?ä | tail]), do: 'ae' ++ sanitize(tail)
-  def sanitize([?ö | tail]), do: 'oe' ++ sanitize(tail)
-  def sanitize([?ü | tail]), do: 'ue' ++ sanitize(tail)
-  def sanitize([?ß | tail]), do: 'ss' ++ sanitize(tail)
-  def sanitize([?_ | tail]), do: '_' ++ sanitize(tail)
+  def sanitize([?ä | tail]), do: ~c"ae" ++ sanitize(tail)
+  def sanitize([?ö | tail]), do: ~c"oe" ++ sanitize(tail)
+  def sanitize([?ü | tail]), do: ~c"ue" ++ sanitize(tail)
+  def sanitize([?ß | tail]), do: ~c"ss" ++ sanitize(tail)
+  def sanitize([?_ | tail]), do: ~c"_" ++ sanitize(tail)
   def sanitize([_ | tail]), do: sanitize(tail)
 end
