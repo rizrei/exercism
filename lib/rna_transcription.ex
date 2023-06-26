b4f0d33c377d594d1749a82870d94781233d3ae9
Albert Gabitov <agabitov@evrone.com>
2023-06-26 20:19:25 +0300

    RnaTranscription(Enum#map)

---
+++ lib/rna_transcription.ex
@@ -0,0 +1,18 @@
+defmodule RnaTranscription do
+  @nucleotide_map %{
+    ?G => ?C,
+    ?C => ?G,
+    ?T => ?A,
+    ?A => ?U
+  }
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RnaTranscription.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna), do: dna |> Enum.map(&@nucleotide_map[&1])
+end
