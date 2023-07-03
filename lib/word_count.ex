e5fd82b4ee49c0a01f30fcc71fce8913e2501dd9
Albert Gabitov <agabitov@evrone.com>
2023-07-03 20:22:16 +0300

    WordCount(String#replace, String#split)

---
+++ lib/word_count.ex
@@ -0,0 +1,17 @@
+defmodule WordCount do
+  @doc """
+  Count the number of words in the sentence.
+
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t()) :: map
+  def count(sentence) do
+    sentence
+    |> String.downcase()
+    |> String.replace(~w[: ! & @ $ % ^ & .], "")
+    |> String.split([" ", ",\n", ",", "\n", "\t", "_"])
+    |> Enum.map(&String.trim(&1, "'"))
+    |> Enum.reject(&(&1 == ""))
+    |> Enum.frequencies()
+  end
+end
