608c5309689c72f18fd810e982969b24172b0843
Albert Gabitov <agabitov@evrone.com>
2023-07-18 19:44:06 +0300

    Pangram(Kernel#--)

---
+++ lib/panagram.ex
@@ -0,0 +1,23 @@
+defmodule Pangram do
+  @doc """
+  Determines if a word or sentence is a pangram.
+  A pangram is a sentence using every letter of the alphabet at least once.
+
+  Returns a boolean.
+
+    ## Examples
+
+      iex> Pangram.pangram?("the quick brown fox jumps over the lazy dog")
+      true
+
+  """
+
+  @alphabet ?a..?z |> Enum.map(&<<&1>>)
+
+  @spec pangram?(String.t()) :: boolean
+  def pangram?(sentence) do
+    sentence_graphemes = sentence |> String.downcase() |> String.graphemes()
+
+    @alphabet |> Kernel.--(sentence_graphemes) |> Enum.empty?()
+  end
+end
