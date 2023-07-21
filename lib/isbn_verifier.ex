4309e1399d613f0a5589e842f0f694597c936829
Albert Gabitov <agabitov@evrone.com>
2023-07-21 21:55:44 +0300

    IsbnVerifier(<<>>, binary matching)

--- lib/isbn_verifier.ex
+++ lib/isbn_verifier.ex
@@ -12,24 +12,13 @@ defmodule IsbnVerifier do
 
   """
   @spec isbn?(String.t()) :: boolean
-  def isbn?(isbn) do
-    formatted_isbn = isbn |> String.replace("-", "")
+  def isbn?(isbn), do: do_isbn?(isbn, 10, 0)
 
-    case formatted_isbn |> String.match?(~r/^[0-9]{9}([0-9]{1}|X)$/) do
-      true -> formatted_isbn |> mod11() |> Kernel.==(0)
-      _ -> false
-    end
-  end
+  def do_isbn?(<<c, rest::binary>>, n, sum) when c in ?0..?9,
+    do: do_isbn?(rest, n - 1, sum + n * (c - ?0))
 
-  defp mod11(formatted_isbn) do
-    formatted_isbn
-    |> String.reverse()
-    |> String.codepoints()
-    |> Enum.with_index(1)
-    |> Enum.reduce(0, fn
-      {"X", i}, acc -> 10 * i + acc
-      {n, i}, acc -> String.to_integer(n) * i + acc
-    end)
-    |> Integer.mod(11)
-  end
+  def do_isbn?(<<c, rest::binary>>, n, sum)when c == ?-, do: do_isbn?(rest, n, sum)
+  def do_isbn?("X", _, sum), do: do_isbn?(<<>>, 0, sum + 10)
+  def do_isbn?(<<>>, 0, sum), do: sum |> Integer.mod(11) == 0
+  def do_isbn?(_, _, _), do: false
 end
