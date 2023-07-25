b57317afc0eb02cfb21d1b5d023f5bbae0ceb73f
Albert Gabitov <agabitov@evrone.com>
2023-07-25 19:59:00 +0300

    BoutiqueSuggestions(for)

---
+++ lib/boutique_suggestions.ex
@@ -0,0 +1,10 @@
+defmodule BoutiqueSuggestions do
+  def get_combinations(tops, bottoms, options \\ []) do
+    max_price = options |> Keyword.get(:maximum_price, 100)
+
+    for %{base_color: top_base_color, price: top_price} = top <- tops,
+        %{base_color: bottom_base_color, price: bottom_price} = bottom <- bottoms,
+        top_base_color !== bottom_base_color and top_price + bottom_price <= max_price,
+        do: {top, bottom}
+  end
+end
