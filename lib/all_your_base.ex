d654a8fcd9cb91cadbf7eaa95fae33631754245b
Albert Gabitov <agabitov@evrone.com>
2026-04-26 00:42:21 +0300

    All Your Base

--- lib/all_your_base.ex
+++ lib/all_your_base.ex
@@ -10,28 +10,23 @@ defmodule AllYourBase do
     invalid_output_base: "output base must be >= 2"
   }
 
-  @spec convert(list(), integer(), integer()) :: {:ok, [integer()]} | {:error, String.t()}
-  def convert(_, input_base, _) when input_base < 2, do: {:error, @errors.invalid_input_base}
-  def convert(_, _, output_base) when output_base < 2, do: {:error, @errors.invalid_output_base}
+  @min_base 2
+
+  @spec convert([integer()], integer(), integer()) :: {:ok, [integer()]} | {:error, String.t()}
+  def convert(_, input_base, _) when input_base < @min_base,
+    do: {:error, @errors.invalid_input_base}
+
+  def convert(_, _, output_base) when output_base < @min_base,
+    do: {:error, @errors.invalid_output_base}
 
   def convert(digits, input_base, output_base) do
-    if Enum.all?(digits, fn num -> num >= 0 and num < input_base end) do
-      {:ok, digits |> to_decimal(input_base) |> from_decimal(output_base)}
+    if Enum.all?(digits, &(0 <= &1 and &1 < input_base)) do
+      digits
+      |> Enum.reduce(0, fn digit, acc -> acc * input_base + digit end)
+      |> Integer.digits(output_base)
+      |> then(&{:ok, &1})
     else
-      {:error, @errors.invalid_digits}
+      {:error, "all digits must be >= 0 and < input base"}
     end
   end
-
-  defp to_decimal(_num, base, _result \\ 0)
-  defp to_decimal([], _, result), do: result
-
-  defp to_decimal([num | rest] = digits, base, result) do
-    to_decimal(rest, base, result + num * base ** (length(digits) - 1))
-  end
-
-  defp from_decimal(_number, _base, _result \\ [])
-  defp from_decimal(number, base, result) when number < base, do: [number | result]
-
-  defp from_decimal(number, base, result),
-    do: from_decimal(div(number, base), base, [rem(number, base) | result])
 end
