84e8efbc894ab6b0e264853ad48526b5a3b28547
Albert Gabitov <agabitov@evrone.com>
2026-05-20 23:40:43 +0300

    Re-implement BookStore total price calculation
    The `BookStore` module's `total/1` function has been re-implemented. The new approach transforms the input basket into a sorted list of book frequencies and utilizes a recursive backtracking algorithm to explore different bundling strategies, ensuring the lowest possible price is found. This replaces the previous memoized solution.

--- lib/book_store.ex
+++ lib/book_store.ex
@@ -1,63 +1,54 @@
 defmodule BookStore do
-  @moduledoc """
-  Calculates the price of a shopping basket with the biggest possible discount.
-  """
-
   @typedoc "A book is represented by its number in the 5-book series"
   @type book :: 1 | 2 | 3 | 4 | 5
   @book_price 800
-  @discounts %{1 => 1.0, 2 => 0.95, 3 => 0.90, 4 => 0.80, 5 => 0.75}
-  @group Map.new(@discounts, fn {size, discount} ->
-           {size, @book_price * size * discount}
-         end)
+  @discount_percentage %{
+    1 => 1,
+    2 => 0.95,
+    3 => 0.90,
+    4 => 0.80,
+    5 => 0.75
+  }
 
-  def total([]), do: 0
-  def total(basket), do: basket |> Enum.frequencies() |> min_price_cached(%{}) |> elem(0) |> round()
-
-  defp min_price_cached(freq, cache) when map_size(freq) == 0, do: {0, cache}
-
-  defp min_price_cached(freq, cache) do
-    key = freq |> Enum.sort() |> Enum.into([])
-
-    case Map.get(cache, key) do
-      nil ->
-        {result, updated_cache} =
-          Map.keys(@group)
-          |> Enum.filter(fn size -> map_size(freq) >= size end)
-          |> Enum.reduce({nil, cache}, fn size, {current_min, curr_cache} ->
-            {recursive_cost, next_cache} =
-              freq |> group_books(size) |> min_price_cached(curr_cache)
-
-            cost = @group[size] + recursive_cost
-
-            new_min =
-              case current_min do
-                nil -> cost
-                min -> min(min, cost)
-              end
-
-            {new_min, next_cache}
-          end)
-
-        {result, Map.put(updated_cache, key, result)}
-
-      cached_result ->
-        {cached_result, cache}
-    end
+  @doc """
+  Calculate lowest price (in cents) for a shopping basket containing books.
+  """
+  @spec total(basket :: [book]) :: integer
+  def total(basket) do
+    basket
+    |> Enum.frequencies()
+    |> Map.values()
+    |> Enum.sort(:desc)
+    |> do_total
   end
 
-  # Remove a group of books with the highest frequencies from the frequency map
-  defp group_books(freq, size) do
-    books_to_remove =
-      freq
-      |> Enum.sort_by(fn {_book, count} -> -count end)
-      |> Enum.take(size)
-      |> Enum.map(&elem(&1, 0))
+  defp do_total([]), do: 0
 
-    Enum.reduce(books_to_remove, freq, fn book, acc ->
-      Map.update!(acc, book, &(&1 - 1))
+  defp do_total(book_occurrencies) do
+    different_books = book_occurrencies |> Enum.count()
+
+    1..different_books
+    |> Enum.map(fn bundle_size ->
+      book_occurrencies
+      |> remaning_book_occurrencies(bundle_size)
+      |> do_total()
+      |> Kernel.+(bundle_price(bundle_size))
     end)
-    |> Enum.reject(fn {_book, count} -> count == 0 end)
-    |> Enum.into(%{})
+    |> Enum.min()
   end
+
+  defp remaning_book_occurrencies(book_occurrencies, 0),
+    do:
+      book_occurrencies
+      |> Enum.sort(:desc)
+
+  defp remaning_book_occurrencies(book_occurrencies, bundle_size),
+    do:
+      book_occurrencies
+      |> List.update_at(bundle_size - 1, &(&1 - 1))
+      |> Enum.reject(&(&1 == 0))
+      |> remaning_book_occurrencies(bundle_size - 1)
+
+  defp bundle_price(bundle_size),
+    do: @discount_percentage[bundle_size] * bundle_size * @book_price
 end
